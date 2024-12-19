a157624af96d4f54495de14248241d0c63389b7c
Manal Mansoor. <manal.mansoor241@gmail.com>
2024-12-19 11:05:57 -0800

    Add files via upload

---
+++ ch 2 js/app.js
@@ -0,0 +1,51 @@
+// Chapter 2
+//Question 1
+//let userName = "Manal";
+//  alert(userName);
+
+
+//Question 2
+// let myName = "Manal Mansoor";
+// console.log(myName)
+
+
+//Question 3
+// let message = "Hello World!!";
+// alert(message);
+
+
+//Question 4
+// let Studentname = "Manal";
+// let Studentage = "20";
+// let Studentcourse = "Web & App Development";
+// alert("Student Name :  " + Studentname);
+// alert("Student Age :  " + Studentage);
+// alert("Student Course :  " + Studentcourse);
+
+
+//Question 5
+// let food = "Pizza";
+// let Pizza = food + "\n" + food.slice(0, 4) + "\n" + food.slice(0, 3) + "\n" + food.slice(0, 2) + "\n" + food.slice(0, 1);
+// alert(Pizza);
+
+
+
+//Question 6
+// let email = "example@example.com";
+// alert(email);
+
+
+//Question 7
+// let message = "A smarter way to learn JavaScript ";
+// alert(message);
+
+
+//Question 8
+// let message = "Yah!! I can write HTML content through JS.";
+// document.write(message);
+
+
+
+//Question 9
+// let message = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬ ";
+// alert(message);
